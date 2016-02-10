f7082a3600f785c290a75db11da23ffd094b4d4d
DevPGSV <DevPGSV@gmail.com>
2016-02-10 19:46:19 +0100

    Made a template. Added doc

--- exams/2012/feb/5/design.cpp
+++ exams/2012/feb/5/design.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int getTop(int vect[], int n);
+template <typename Type>
+Type getTop(Type vect[], int n);
 
 int main() {
 	int vect[] = {1, 6, 7, 8, 15, 14, 6, 1};
@@ -10,8 +11,19 @@ int main() {
 	return 0;
 }
 
-int getTop(int vect[], int n) {
-	int top = vect[0];
+/**
+ * <p>Get the maximum value of the vector <i>vect</i> of <i>n</i> elements of type <i>Type</i></p>
+ * <p>The type <i>Type</i> has to be sortable</p>
+ * <p>The vector <i>vect</i> has to be "mountain"
+ * 
+ * @param vect input vector (mountain)
+ * @param n the size of <i>vect</i>
+ *
+ * @return The top of the mountain, ie. the maximum value in <i>vect</i>
+ */
+template <typename Type>
+Type getTop(Type vect[], int n) {
+	Type top = vect[0];
 
 	int i = 0;
 	while (i < n) {
