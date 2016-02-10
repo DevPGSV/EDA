05c929c617a399ae130769df75e80c79e4d7d601
DevPGSV <DevPGSV@gmail.com>
2016-02-10 19:35:07 +0100

    exam feb, 2012; ex 5; added design

--- exams/2012/feb/5/design.cpp
+++ exams/2012/feb/5/design.cpp
@@ -1,1 +1,25 @@
-//WIP
+#include <iostream>
+
+int getTop(int vect[], int n);
+
+int main() {
+	int vect[] = {1, 6, 7, 8, 15, 14, 6, 1};
+
+	std::cout << getTop(vect, sizeof(vect) / sizeof(int)) << std::endl;
+
+	return 0;
+}
+
+int getTop(int vect[], int n) {
+	int top = vect[0];
+
+	int i = 0;
+	while (i < n) {
+		if (vect[i] > top) {
+			top = vect[i];
+		}
+		i++;
+	}
+
+	return top;
+}
