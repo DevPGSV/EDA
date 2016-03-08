6ce955583a0c8fe0da041699ef665b3da3a452fb
DevPGSV <DevPGSV@gmail.com>
2016-03-08 20:57:07 +0100

    Added code for exer 1, sec 8

---
+++ exercises/8.Lineal TADs/1/Source1.cpp
@@ -0,0 +1,36 @@
+/*
+ * Source8.cpp
+ *
+ *  Created on: Mar 8, 2016
+ *      Author: DevPGSV
+ */
+
+
+#include <iostream>
+
+int main() {
+	char inChar;
+	int sum, input;
+
+	std::cin.get(inChar);
+	while (inChar != '\n' && inChar != '-' && !std::cin.eof()) {
+		sum = 0;
+
+		input = inChar - '0';
+		sum += input;
+		std::cout << input;
+		std::cin.get(inChar);
+
+		while (inChar != '\n' && inChar != '-' && !std::cin.eof()) {
+			input = inChar - '0';
+			sum += input;
+			std::cout << " + " << input;
+			std::cin.get(inChar);
+		}
+		std::cout << " = " << sum << std::endl;
+
+		std::cin.get(inChar);
+	}
+
+	return 0;
+}
